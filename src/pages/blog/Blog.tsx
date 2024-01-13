d32af7f425c21b0b49e553f027e70a7a44420f91
aizaz-dev <azazkhan2682000@gmail.com>
2024-01-13 09:14:04 -0800

    commented components first 11 pages

--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,6 +1,6 @@
 import { FaAngleRight } from "react-icons/fa6";
 import { useTranslation } from "react-i18next";
-
+// Blo Component
 const Blog = () => {
   const { t } = useTranslation();
 
@@ -8,14 +8,13 @@ const Blog = () => {
     <div className="max-w-[2000px] mx-auto py-[50px]  w-[100%] flex flex-col lg:px-[80px] px-[20px] ">
       <div className="w-[100%] flex items-center lg:gap-6 md:gap-4 gap-3 flex-col lg:mt-[50px] md:mt-[30px] mt-[20px]">
         <h1 className="font-[Outfit-Bold] w-[93%] mb-[60px] text-left text-lg   leading-[60px] max-laptop:text-lf max-md:text-sm max-md:leading-8 max-md:text-center max-laptop:font-[700] max-laptop:leading-10">
-        {t("blogTitle")}
-
+          {/* Title Section */}
+          {t("blogTitle")}
         </h1>
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section1.title")}
-
+              {t("section1.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -26,8 +25,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section2.title")}
-
+              {t("section2.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -38,8 +36,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -50,8 +47,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -61,8 +57,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -72,8 +67,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -83,8 +77,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -94,8 +87,7 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -105,8 +97,7 @@ const Blog = () => {
         <div className="w-[93%]  md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]   text-light200">
@@ -116,16 +107,13 @@ const Blog = () => {
         <div className="w-[93%] lg:h-[100px] md:h-[80px] h-60px bg-light100  rounded-[5px] flex lg:px-[50px] md:px-[20px] px-[10px] py-[10px] justify-between shadow-md  items-center">
           <div>
             <div className="font-[Outfit-Bold] text-left text-sm  max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-            {t("section3.title")}
-
+              {t("section3.title")}
             </div>
           </div>
           <div className="text-[40px]  text-light200">
             <FaAngleRight />
           </div>
         </div>
-
-        
       </div>
     </div>
   );
