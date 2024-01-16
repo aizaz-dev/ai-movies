630dd1ad2bc20ad0748adb987dd6113b9d57460e
aizaz-dev <azazkhan2682000@gmail.com>
2024-01-16 16:56:29 +0500

    faq to bottom

--- src/components/Faqs.tsx
+++ src/components/Faqs.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import { AiOutlineDown, AiOutlineUp } from "react-icons/ai";
 
 const Faq = () => {
   const { t } = useTranslation();
 
-  // State to track the active question
-  const [activeQuestion, setActiveQuestion] = useState(0);
+  // State to track the active question and arrow direction
+  const [activeQuestion, setActiveQuestion] = useState(null);
+  const [arrowUp, setArrowUp] = useState(false);
 
   // Array containing FAQ data with questions and answers
   const faqData = [
@@ -38,18 +40,19 @@ const Faq = () => {
   ];
 
   const handleQuestionClick = (index) => {
-    setActiveQuestion(index === activeQuestion ? null : index);
+    setActiveQuestion(activeQuestion === index ? null : index);
+    setArrowUp(!arrowUp);
   };
 
   return (
-    <div className="w-full bg-light100 px-[80px] py-[80px] max-tab:px-[40px] max-md:px-[30px] ">
+    <div className="w-full bg-light100 px-[80px] py-[80px] max-tab:px-[40px] max-md:px-[20px] ">
       <div className="max-w-[2000px] m-auto">
-        <h3 className="text-lf font-[Outfit-Bold]  max-md:text-center max-laptop:text-lf">
+        <h3 className="text-lf font-[Outfit-Bold] max-md:text-center max-laptop:text-lf">
           {t("faq.faqHeading")}
         </h3>
 
-        <div className="faq mt-7 flex max-md:flex-col mx-auto py-6">
-          <div className="one w-[50%] max-md:w-full rounded-[10px] bg-light400 shadow-md">
+        <div className="faq mt-7 flex flex-col md:flex-row">
+          <div className="one w-[50%]  max-tab:w-full max-md:w-full rounded-[10px] bg-light400 shadow-md">
             {faqData.map((faq, index) => (
               <div
                 key={index}
@@ -60,16 +63,30 @@ const Faq = () => {
                 } cursor-pointer`}
                 onClick={() => handleQuestionClick(index)}
               >
-                <p className="font-[Outfit-Bold] text-primary text-left text-sm max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
-                  {faq.question}
-                </p>
+                <div className="flex items-center justify-between max-tab:items-start max-tab:justify-between">
+                  <p className="font-[Outfit-Bold] text-primary text-left text-sm max-tab:w-auto max-md:text-mf max-laptop:text-mf flex items-start justify-start max-md:text-start">
+                    {faq.question}
+                  </p>
+                  <div className=" flex items-end justify-end">
+                  {activeQuestion === index ? (
+                    <AiOutlineUp className="text-primary cursor-pointer text-[20px] hidden max-tab:block mt-1" />
+                  ) : (
+                    <AiOutlineDown className="text-primary cursor-pointer text-[20px] hidden max-tab:block max-md:mt-1" />
+                  )}
+                  </div>
+                </div>
+                {activeQuestion === index && (
+                  <p className="font-[Outfit-Regular]  hidden max-tab:block text-left text-sm max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start pt-2">
+                    {faq.answer}
+                  </p>
+                )}
               </div>
             ))}
           </div>
 
-          <div className="two p-10 w-[50%] max-md:w-full">
+          <div className="two w-[50%] max-tab:hidden p-10  ">
             {activeQuestion !== null && (
-              <p className="font-[Outfit-Regular] text-left text-sm max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
+              <p className="tabans font-[Outfit-Regular] text-left text-sm max-tab:w-auto max-md:text-mf max-laptop:text-mf max-md:text-start">
                 {faqData[activeQuestion].answer}
               </p>
             )}
